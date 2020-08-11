010f8847845b0ea2a672eea88fb3ff6c8e353cfc
CeeDeeBee <cdbarnes98@gmail.com>
2020-08-11 18:13:00 -0400

    Add improved day 2 solution

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,8 +1,9 @@
-verticies = {}
+from util import Stack, Queue
 
 
 def earliest_ancestor(ancestors, starting_node):
     # initialize graph
+    verticies = {}
     for edge in ancestors:
         if edge[1] in verticies.keys():
             verticies[edge[1]].append(edge[0])
@@ -10,18 +11,54 @@ def earliest_ancestor(ancestors, starting_node):
             verticies[edge[1]] = [edge[0]]
 
     # sort sets to ensure lowest vertex is always checked first
-    for key in verticies.keys():
-        verticies[key].sort()
+    # for key in verticies.keys():
+    #     verticies[key].sort()
 
-    def dfs(current_vertex):
-        # verticies with no ancestors will not be in the verticies dict
-        if current_vertex not in verticies.keys():
-            if current_vertex == starting_node:
+    # highest_depth = 0
+    # deepest_val = 0
+    # current_depth = 0
+    # s = Stack()
+    # s.push(starting_node)
+
+    # while s.size() > 0:
+    #     vertex = s.pop()
+
+    #     if vertex not in verticies.keys():
+    #         if current_depth > highest_depth:
+    #             deepest_val = vertex
+    #             highest_depth = current_depth
+
+    #         if vertex == starting_node:
+    #             return -1
+
+    #     else:
+    #         current_depth += 1
+    #         for n in verticies[vertex]:
+    #             s.push(n)
+
+    # return deepest_val
+
+    s = Stack()
+    s.push([starting_node])
+    deepest_path = []
+
+    while s.size() > 0:
+        path = s.pop()
+        vertex = path[-1]
+
+        if vertex not in verticies.keys():
+            if vertex == starting_node:
                 return -1
 
-            return current_vertex
+            if len(path) > len(deepest_path):
+                deepest_path = path
+            elif len(path) == len(deepest_path):
+                if vertex < deepest_path[-1]:
+                    deepest_path = path
+        else:
+            for n in verticies[vertex]:
+                new_path = path.copy()
+                new_path.append(n)
+                s.push(new_path)
 
-        for n in verticies[current_vertex]:
-            return dfs(n)
-
-    return dfs(starting_node)
+    return deepest_path[-1]
